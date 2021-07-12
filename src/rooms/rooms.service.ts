e454cc7c0b35af32cb4a13932cadc7242ff444ec
Sergey <sergios33033@gmail.com>
2021-07-12 15:57:50 +0300

    All methods wrapped in try-catch, input params and return types changed, function for the rights verification added, the InternalException changed to the RpcException.

--- src/rooms/rooms.service.ts
+++ src/rooms/rooms.service.ts
@@ -1,13 +1,13 @@
 import { HttpStatus, Injectable } from "@nestjs/common";
+import { RpcException } from "@nestjs/microservices";
 import { InjectModel } from "@nestjs/mongoose";
-import { Request } from "express";
+import { Observable } from "rxjs";
 import { Model } from "mongoose";
 import { GlobalErrorCodes } from "../exceptions/errorCodes/GlobalErrorCodes";
-import { InternalException } from "../exceptions/Internal.exception";
+import { Rights } from "../utils/rights";
 import { RightsDocument } from "./schemas/rights.schema";
 import { RoomDocument } from "./schemas/room.schema";
-import { RoomDto } from "./dto/room.dto";
-import { Rights } from "../utils/rights";
+import { RoomDto } from "./room.dto";
 
 @Injectable()
 export class RoomsService {
@@ -16,145 +16,291 @@ export class RoomsService {
     @InjectModel("Rights") private readonly rightsModel: Model<RightsDocument>
   ) {}
 
-  // @MessagePattern("create-room")
-  async createRoom(roomDto: RoomDto): Promise<HttpStatus> {
-    const createdRoom = new this.roomModel(roomDto);
+  async createRoom(roomDto: RoomDto): Promise<HttpStatus | Observable<any> | RpcException> {
+    try {
+      const createdRoom = new this.roomModel(roomDto);
 
-    await createdRoom.save();
-    return HttpStatus.CREATED;
-  }
-
-  async getAllRooms(): Promise<RoomDocument[]> {
-    return this.roomModel.find();
-  }
-
-  async findRoomById(req: Request): Promise<RoomDocument[] | null> {
-    const regex = new RegExp(req.params.id, "i");
-    return this.roomModel.find({ id: regex });
-  }
-
-  async findRoomByName(req: Request): Promise<RoomDocument[] | null> {
-    const regex = new RegExp(req.params.name, "i");
-    return this.roomModel.find({ name: regex });
-    // return this.roomModel.findOne({ name: req.params.name });
-  }
-
-  async updateRoom(req: Request, roomDto: Partial<RoomDto>): Promise<HttpStatus> {
-    if (req.rights.includes("CHANGE_ROOM")) {
-      const room = await this.roomModel.findOne({ id: req.params.id });
-
-      const updatedRoom = {
-        usersID: roomDto.usersID ? roomDto.usersID : room.usersID,
-        messagesID: roomDto.messagesID ? roomDto.messagesID : room.messagesID,
-        _id: room._id,
-        id: req.params.id,
-        name: roomDto.name ? roomDto.name : room.name,
-        description: roomDto.description ? roomDto.description : room.description,
-        isUser: roomDto.isUser ? roomDto.isUser : room.isUser,
-        isPrivate: roomDto.isPrivate ? roomDto.isPrivate : room.isPrivate,
-        membersCount: roomDto.membersCount ? roomDto.membersCount : room.membersCount,
-        createdAt: room.createdAt,
-        updatedAt: new Date()
-      };
-      await this.roomModel.updateOne({ id: req.params.id }, updatedRoom);
+      await createdRoom.save();
       return HttpStatus.CREATED;
+    } catch (e) {
+      console.log(e.stack);
+      return new RpcException({
+        key: "INTERNAL_ERROR",
+        code: GlobalErrorCodes.INTERNAL_ERROR.code,
+        message: GlobalErrorCodes.INTERNAL_ERROR.value
+      });
     }
-    return HttpStatus.UNAUTHORIZED;
   }
 
-  async deleteRoom(req: Request): Promise<HttpStatus> {
-    if (req.rights.includes("DELETE_ROOM")) {
-      const { deletedCount } = await this.roomModel.deleteOne({ id: req.params.id });
-
-      if (deletedCount !== 0) {
-        return HttpStatus.OK;
-      } else {
-        return HttpStatus.NOT_FOUND;
-      }
+  async getAllRooms(): Promise<RoomDocument[] | Observable<any> | RpcException> {
+    try {
+      return this.roomModel.find();
+    } catch (e) {
+      console.log(e.stack);
+      return new RpcException({
+        key: "INTERNAL_ERROR",
+        code: GlobalErrorCodes.INTERNAL_ERROR.code,
+        message: GlobalErrorCodes.INTERNAL_ERROR.value
+      });
     }
-    return HttpStatus.UNAUTHORIZED;
   }
 
-  async deleteMessageFromRoom(req: Request, messageId: string, roomId: string): Promise<HttpStatus> {
-    if (req.rights.includes("DELETE_MESSAGES")) {
-      const searchResult = await this.roomModel.findOne({ id: roomId });
+  async findRoomById(id: string): Promise<RoomDocument[] | Observable<any> | RpcException> {
+    try {
+      const regex = new RegExp(id, "i");
+      return this.roomModel.find({ id: regex });
+    } catch (e) {
+      console.log(e.stack);
+      return new RpcException({
+        key: "INTERNAL_ERROR",
+        code: GlobalErrorCodes.INTERNAL_ERROR.code,
+        message: GlobalErrorCodes.INTERNAL_ERROR.value
+      });
+    }
+  }
 
-      const messagePosition = searchResult.messagesID.findIndex((item) => item === messageId);
+  async findRoomByName(name: string): Promise<RoomDocument[] | Observable<any> | RpcException> {
+    try {
+      const regex = new RegExp(name, "i");
+      return this.roomModel.find({ name: regex });
+    } catch (e) {
+      console.log(e.stack);
+      return new RpcException({
+        key: "INTERNAL_ERROR",
+        code: GlobalErrorCodes.INTERNAL_ERROR.code,
+        message: GlobalErrorCodes.INTERNAL_ERROR.value
+      });
+    }
+  }
 
-      if (messagePosition > -1) {
-        searchResult.messagesID.splice(messagePosition, 1);
-        await this.roomModel.updateOne({ id: roomId }, searchResult);
+  async updateRoom({
+    rights,
+    roomId,
+    roomDto
+  }: {
+    rights: string[];
+    roomId: string;
+    roomDto: Partial<RoomDto>;
+  }): Promise<HttpStatus | Observable<any> | RpcException> {
+    try {
+      if (rights.includes("CHANGE_ROOM")) {
+        const room = await this.roomModel.findOne({ id: roomId });
+
+        const updatedRoom = {
+          usersID: roomDto.usersID ? roomDto.usersID : room.usersID,
+          messagesID: roomDto.messagesID ? roomDto.messagesID : room.messagesID,
+          _id: room._id,
+          id: roomId,
+          name: roomDto.name ? roomDto.name : room.name,
+          description: roomDto.description ? roomDto.description : room.description,
+          isUser: roomDto.isUser ? roomDto.isUser : room.isUser,
+          isPrivate: roomDto.isPrivate ? roomDto.isPrivate : room.isPrivate,
+          membersCount: roomDto.membersCount ? roomDto.membersCount : room.membersCount,
+          createdAt: room.createdAt,
+          updatedAt: new Date()
+        };
+        await this.roomModel.updateOne({ id: roomId }, updatedRoom);
         return HttpStatus.CREATED;
-      } else {
-        return HttpStatus.NOT_FOUND;
       }
+      return HttpStatus.UNAUTHORIZED;
+    } catch (e) {
+      console.log(e.stack);
+      return new RpcException({
+        key: "INTERNAL_ERROR",
+        code: GlobalErrorCodes.INTERNAL_ERROR.code,
+        message: GlobalErrorCodes.INTERNAL_ERROR.value
+      });
     }
-    return HttpStatus.UNAUTHORIZED;
   }
 
-  async addMessageReferenceToRoom(req: Request, messageId: string, roomId: string): Promise<HttpStatus> {
-    if (req.rights.includes("SEND_MESSAGES")) {
-      const searchResult = await this.roomModel.findOne({ id: roomId });
+  async deleteRoom({ rights, roomId }: { rights: string[]; roomId: string }): Promise<HttpStatus | Observable<any> | RpcException> {
+    try {
+      if (rights.includes("DELETE_ROOM")) {
+        const { deletedCount } = await this.roomModel.deleteOne({ id: roomId });
 
-      searchResult.messagesID.push(messageId);
-
-      await this.roomModel.updateOne({ id: roomId }, searchResult);
-
-      return HttpStatus.CREATED;
+        if (deletedCount !== 0) {
+          return HttpStatus.OK;
+        } else {
+          return HttpStatus.NOT_FOUND;
+        }
+      }
+      return HttpStatus.UNAUTHORIZED;
+    } catch (e) {
+      console.log(e.stack);
+      return new RpcException({
+        key: "INTERNAL_ERROR",
+        code: GlobalErrorCodes.INTERNAL_ERROR.code,
+        message: GlobalErrorCodes.INTERNAL_ERROR.value
+      });
     }
-    return HttpStatus.UNAUTHORIZED;
   }
 
-  async addUserToRoom(req: Request, userId: string, roomId: string): Promise<HttpStatus> {
-    if (req.rights.includes("ADD_USERS")) {
-      const searchResult = await this.roomModel.findOne({ id: roomId });
+  async deleteMessageFromRoom({
+    rights,
+    roomId,
+    messageId
+  }: {
+    rights: string[];
+    roomId: string;
+    messageId: string;
+  }): Promise<HttpStatus | Observable<any> | RpcException> {
+    try {
+      if (rights.includes("DELETE_MESSAGES")) {
+        const searchResult = await this.roomModel.findOne({ id: roomId });
 
-      searchResult.usersID.push(userId);
+        const messagePosition = searchResult.messagesID.findIndex((item) => item === messageId);
 
-      await this.roomModel.updateOne({ id: roomId }, searchResult);
-
-      return HttpStatus.CREATED;
+        if (messagePosition > -1) {
+          searchResult.messagesID.splice(messagePosition, 1);
+          await this.roomModel.updateOne({ id: roomId }, searchResult);
+          return HttpStatus.CREATED;
+        } else {
+          return HttpStatus.NOT_FOUND;
+        }
+      }
+      return HttpStatus.UNAUTHORIZED;
+    } catch (e) {
+      console.log(e.stack);
+      return new RpcException({
+        key: "INTERNAL_ERROR",
+        code: GlobalErrorCodes.INTERNAL_ERROR.code,
+        message: GlobalErrorCodes.INTERNAL_ERROR.value
+      });
     }
-    return HttpStatus.UNAUTHORIZED;
   }
 
-  async deleteUserFromRoom(req: Request, userId: string, roomId: string): Promise<HttpStatus> {
-    if (req.rights.includes("DELETE_USERS")) {
-      const searchResult = await this.roomModel.findOne({ id: roomId });
+  async addMessageReferenceToRoom({
+    rights,
+    messageId,
+    roomId
+  }: {
+    rights: string[];
+    messageId: string;
+    roomId: string;
+  }): Promise<HttpStatus | Observable<any> | RpcException> {
+    try {
+      if (rights.includes("SEND_MESSAGES")) {
+        const searchResult = await this.roomModel.findOne({ id: roomId });
 
-      const userPosition = searchResult.usersID.findIndex((item) => item === userId);
+        searchResult.messagesID.push(messageId);
 
-      if (userPosition > -1) {
-        searchResult.usersID.splice(userPosition, 1);
         await this.roomModel.updateOne({ id: roomId }, searchResult);
+
         return HttpStatus.CREATED;
-      } else {
-        return HttpStatus.NOT_FOUND;
       }
+      return HttpStatus.UNAUTHORIZED;
+    } catch (e) {
+      console.log(e.stack);
+      return new RpcException({
+        key: "INTERNAL_ERROR",
+        code: GlobalErrorCodes.INTERNAL_ERROR.code,
+        message: GlobalErrorCodes.INTERNAL_ERROR.value
+      });
     }
-    return HttpStatus.UNAUTHORIZED;
   }
 
-  async changeUserRightsInRoom(req: Request, userId: string, roomId: string, newRights: string[]): Promise<HttpStatus> {
-    if (req.rights.includes("CHANGE_USER_RIGHTS")) {
-      await this.rightsModel.updateOne({ id: userId, roomId }, { rights: newRights });
+  async addUserToRoom({
+    rights,
+    userId,
+    roomId
+  }: {
+    rights: string[];
+    userId: string;
+    roomId: string;
+  }): Promise<HttpStatus | Observable<any> | RpcException> {
+    try {
+      if (rights.includes("ADD_USERS")) {
+        const searchResult = await this.roomModel.findOne({ id: roomId });
 
-      return HttpStatus.CREATED;
+        searchResult.usersID.push(userId);
+
+        await this.roomModel.updateOne({ id: roomId }, searchResult);
+
+        return HttpStatus.CREATED;
+      }
+      return HttpStatus.UNAUTHORIZED;
+    } catch (e) {
+      console.log(e.stack);
+      return new RpcException({
+        key: "INTERNAL_ERROR",
+        code: GlobalErrorCodes.INTERNAL_ERROR.code,
+        message: GlobalErrorCodes.INTERNAL_ERROR.value
+      });
     }
-    return HttpStatus.UNAUTHORIZED;
   }
 
-  async verifyRights(req: Request): Promise<boolean> {
-    let rights: typeof Rights;
+  async deleteUserFromRoom({
+    rights,
+    userId,
+    roomId
+  }: {
+    rights: string[];
+    userId: string;
+    roomId: string;
+  }): Promise<HttpStatus | Observable<any> | RpcException> {
+    try {
+      if (rights.includes("DELETE_USERS")) {
+        const searchResult = await this.roomModel.findOne({ id: roomId });
 
-    if (typeof req.headers["rights"] === "string") {
-      rights = JSON.parse(req.headers["rights"].toString());
-    } else if (Array.isArray(req.headers["rights"])) {
-      rights = req.headers["rights"];
+        const userPosition = searchResult.usersID.findIndex((item) => item === userId);
+
+        if (userPosition > -1) {
+          searchResult.usersID.splice(userPosition, 1);
+          await this.roomModel.updateOne({ id: roomId }, searchResult);
+          return HttpStatus.CREATED;
+        } else {
+          return HttpStatus.NOT_FOUND;
+        }
+      }
+      return HttpStatus.UNAUTHORIZED;
+    } catch (e) {
+      console.log(e.stack);
+      return new RpcException({
+        key: "INTERNAL_ERROR",
+        code: GlobalErrorCodes.INTERNAL_ERROR.code,
+        message: GlobalErrorCodes.INTERNAL_ERROR.value
+      });
     }
+  }
 
-    // return await this.rightsModel.exists({ userId: req.user.userId, rights: rights });
-    req.rights = rights;
-    return true;
+  async changeUserRightsInRoom({
+    rights,
+    userId,
+    roomId,
+    newRights
+  }: {
+    rights: string[];
+    userId: string;
+    roomId: string;
+    newRights: string[];
+  }): Promise<HttpStatus | Observable<any> | RpcException> {
+    try {
+      if (rights.includes("CHANGE_USER_RIGHTS")) {
+        await this.rightsModel.updateOne({ id: userId, roomId }, { rights: newRights });
+
+        return HttpStatus.CREATED;
+      }
+      return HttpStatus.UNAUTHORIZED;
+    } catch (e) {
+      console.log(e.stack);
+      return new RpcException({
+        key: "INTERNAL_ERROR",
+        code: GlobalErrorCodes.INTERNAL_ERROR.code,
+        message: GlobalErrorCodes.INTERNAL_ERROR.value
+      });
+    }
+  }
+
+  async verifyRights(rights: string[], userId: string): Promise<boolean | Observable<any> | RpcException> {
+    try {
+      return await this.rightsModel.exists({ userId: userId, rights: rights });
+    } catch (e) {
+      console.log(e.stack);
+      return new RpcException({
+        key: "INTERNAL_ERROR",
+        code: GlobalErrorCodes.INTERNAL_ERROR.code,
+        message: GlobalErrorCodes.INTERNAL_ERROR.value
+      });
+    }
   }
 }
