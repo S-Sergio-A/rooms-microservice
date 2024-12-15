3952196272af5ad411a5f18dd08b35b06a815a4e
s.smovzh <sergios33033@gmail.com>
2024-12-15 16:52:21 +0200

    fix: update lib

--- src/modules/common/config/configuration.ts
+++ src/modules/common/config/configuration.ts
@@ -1,27 +1,32 @@
 import * as process from "process";
 import * as dotenv from "dotenv";
-import { CloudinaryConfigInterface } from "@ssmovzh/chatterly-common-utils";
+import {
+  AppConfigInterface,
+  CloudinaryConfigInterface,
+  MongoConfigInterface,
+  RabbitConfigInterface
+} from "@ssmovzh/chatterly-common-utils";
 
 dotenv.config();
 
 export default () => ({
   app: {
-    port: process.env.PORT,
+    port: +process.env.PORT,
     environment: process.env.ENVIRONMENT,
     clientUrl: process.env.CLIENT_URL
-  },
+  } as AppConfigInterface,
   mongoConfig: {
     username: process.env.MONGO_USERNAME,
     password: process.env.MONGO_PASSWORD,
     clusterUrl: process.env.MONGO_CLUSTER_URL
-  },
+  } as MongoConfigInterface,
   rabbitConfig: {
     protocol: "amqp",
     hostname: process.env.RABBIT_HOST,
     port: process.env.RABBIT_PORT ? +process.env.RABBIT_PORT : 5672,
     username: process.env.RABBIT_USERNAME,
     password: process.env.RABBIT_PASSWORD
-  },
+  } as RabbitConfigInterface,
   cloudinaryConfig: {
     apiKey: process.env.CLOUDINARY_API_KEY,
     apiSecret: process.env.CLOUDINARY_API_SECRET,
