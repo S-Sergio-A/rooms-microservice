c15e2d556b8efd65ea82f3501422b224c71644e0
s.smovzh <sergios33033@gmail.com>
2024-12-17 01:03:07 +0200

    fix: update mongo url

--- src/app.module.ts
+++ src/app.module.ts
@@ -13,19 +13,19 @@ import { ConnectionNamesEnum, HealthCheckModule, LoggerModule } from "@ssmovzh/c
     HealthCheckModule,
     LoggerModule,
     MongooseModule.forRoot(
-      `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_CLUSTER_URL}/${ConnectionNamesEnum.USERS}?retryWrites=true&w=majority`,
+      `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_CLUSTER_URL}/${ConnectionNamesEnum.USERS}?retryWrites=true&w=majority&appName=Cluster0`,
       {
         connectionName: ConnectionNamesEnum.USERS
       }
     ),
     MongooseModule.forRoot(
-      `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_CLUSTER_URL}/${ConnectionNamesEnum.ROOMS}?retryWrites=true&w=majority`,
+      `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_CLUSTER_URL}/${ConnectionNamesEnum.ROOMS}?retryWrites=true&w=majority&appName=Cluster0`,
       {
         connectionName: ConnectionNamesEnum.ROOMS
       }
     ),
     MongooseModule.forRoot(
-      `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_CLUSTER_URL}/${ConnectionNamesEnum.MESSAGES}?retryWrites=true&w=majority`,
+      `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_CLUSTER_URL}/${ConnectionNamesEnum.MESSAGES}?retryWrites=true&w=majority&appName=Cluster0`,
       {
         connectionName: ConnectionNamesEnum.MESSAGES
       }
